84996b8dacd9aa3ba1f5e04a32da34f7e493ceae
Aathmiga02 <aathmigaloges@gmail.com>
2026-07-19 18:27:55 +0530

    Enhance portfolio loading message and add click events

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Portfolio Loaded");
+window.onload = function () {
+    console.log("Portfolio Loaded Successfully");
+};
+
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach(link => {
+    link.addEventListener("click", function(){
+        console.log(this.innerText + " clicked");
+    });
+});
